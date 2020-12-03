f785effa07da6e381439a3d448e104f10d2dea86
Obi-Wan <cayton10@live.marshall.edu>
2020-12-03 15:12:39 -0500

    Hello World.... w/ Java!

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author BPC
+ */
+
+//First java programming experience!
+import java.lang.System;
+
+//Everything in Java MUST contained in a class
+public class Main 
+{
+    //Entry point
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+    }
+    
+}
